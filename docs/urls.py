271a46a829d60ca388ab552a323beeb2ac192a02
Patrick Upson <upsonp@gmail.com>
2023-09-19 16:41:44 -0300

    User documentation update

--- docs/urls.py
+++ docs/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name="index"),
     path('mission/mission_filter', views.MissionFilter.as_view(), name="mission_filter"),
     path('mission/new_mission_form', views.NewMissionForm.as_view(), name="new_mission_form"),
+    path('mission/mission_events', views.MissionEvents.as_view(), name="mission_events"),
+    path('mission/mission_samples', views.MissionSamples.as_view(), name="mission_samples"),
 ]
