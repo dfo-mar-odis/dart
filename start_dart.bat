3ff34a68ea2841df676154f7ced0140e9169eea0
Patrick Upson <upsonp@gmail.com>
2024-01-20 07:28:16 -0400

    fixed start_dart so 'NULL' file is removed if created

--- start_dart.bat
+++ start_dart.bat
@@ -10,10 +10,12 @@ REM if this is not a git repo, and the application was installed from zip file w
 REM if this is a cloned version of the git repo we want to pull from master, then run the update
 
 git branch | find "* master" > NULL & if ERRORLEVEL 1 (
+	del NULL
 	call .\update.bat
 ) else (
 	echo "Updating Application"
 	git pull origin master >> logs/start_dart.log
 
+	del NULL
 	call .\update.bat
 )
