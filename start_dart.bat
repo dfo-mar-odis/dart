4a073612d0d2c4bfcdc1799e0d8831edcee3a6c3
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2024-04-25 10:32:05 -0300

    updated version numbers

--- start_dart.bat
+++ start_dart.bat
@@ -4,7 +4,7 @@ if not exist ".\logs\" (
   mkdir logs
 )
 
-set dart_version=3.2.0
+set dart_version=3.2.1
 
 REM if this is not a git repo, and the application was installed from zip file we just want to run update
 REM if this is a cloned version of the git repo we want to pull from master, then run the update
