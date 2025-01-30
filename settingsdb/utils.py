567a5ec28c36de8e6d15a09e6b4ed93a9067fd08
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2025-01-30 15:30:06 -0400

    fixed a small bug with Dart not finding biochem fixtures name #246

--- settingsdb/utils.py
+++ settingsdb/utils.py
@@ -26,6 +26,7 @@ def load_biochem_fixtures(database):
         if 'bio_tables_bcupdate' in connections[database].introspection.table_names():
             # check the bio_chem fixture file. If it's been modified then automatically reload the fixtures.
             fixture_file = os.path.join(settings.BASE_DIR, 'bio_tables/fixtures/biochem_fixtures.json')
+
             load_bio_fixtures = 'biochem_fixtures'
             if not os.path.exists(fixture_file):
                 fixture_file = os.path.join(settings.BASE_DIR, 'bio_tables/fixtures/default_biochem_fixtures.json')
