cff316a2b8fbb0ec5b034860e562f8f605880cd6
Patrick Upson <upsonp@gmail.com>
2025-01-31 08:11:04 -0400

    Revert "4.1.x"

--- start_dart.bat
+++ start_dart.bat
@@ -4,16 +4,18 @@ if not exist ".\logs\" (
   mkdir logs
 )
 
-set master_branch=4.1.x
-set dart_version=4.1.0
+set dart_version=4.0.22
 
 REM if this is not a git repo, and the application was installed from zip file we just want to run update
 REM if this is a cloned version of the git repo we want to pull from master, then run the update
 
-git branch | find "* %master_branch%" > NULL & if not ERRORLEVEL 1 (
+git branch | find "* master" > NULL & if ERRORLEVEL 1 (
+	del NULL
+	call .\update.bat
+) else (
 	echo "Updating Application"
-	git pull origin %master_branch% >> logs/start_dart.log
-)
+	git pull origin master >> logs/start_dart.log
 
-del NULL
-call .\update.bat
+	del NULL
+	call .\update.bat
+)
