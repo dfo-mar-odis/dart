70e97d1d6fb3d03805672888f486f5c26f9ad1f4
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2025-01-31 06:57:08 -0400

    Creating Dart version for python 3.13

--- start_dart.bat
+++ start_dart.bat
@@ -4,18 +4,16 @@ if not exist ".\logs\" (
   mkdir logs
 )
 
+set master_branch=4.1.x
 set dart_version=4.1.0
 
 REM if this is not a git repo, and the application was installed from zip file we just want to run update
 REM if this is a cloned version of the git repo we want to pull from master, then run the update
 
-git branch | find "* master" > NULL & if ERRORLEVEL 1 (
-	del NULL
-	call .\update.bat
-) else (
+git branch | find "* %master_branch%" > NULL & if not ERRORLEVEL 1 (
 	echo "Updating Application"
-	git pull origin master >> logs/start_dart.log
-
-	del NULL
-	call .\update.bat
+	git pull origin %master_branch% >> logs/start_dart.log
 )
+
+del NULL
+call .\update.bat
