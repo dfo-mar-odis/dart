b5a27fefb70746d660fc8c4ace29877ac4bf21ad
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2025-06-04 09:34:27 -0300

    fixed a format error

--- update.bat
+++ update.bat
@@ -8,7 +8,7 @@ REM If this was run from a clone repo we can force an update of the python libra
 REM migration on the database by changing the update version
 
 set /p update_version=<current_version.txt
-echo %dart_version% > current_version.txt
+echo %dart_version%> current_version.txt
 
 set first_run=0
 set server_path=.\dart_env\Scripts\activate.bat
