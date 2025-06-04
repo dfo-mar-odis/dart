59ff85683b97b8b146b1294f7cb3e7a4b035ed2b
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2025-06-04 10:23:13 -0300

    updated start script

--- start_dart.bat
+++ start_dart.bat
@@ -7,6 +7,10 @@ if not exist ".\logs\" (
 set master_branch=master
 set /p dart_version=<current_version.txt
 
+echo ============================================ >> logs/start_dart.log
+echo Version: %dart_version >> logs/start_dart.log
+echo ============================================ >> logs/start_dart.log
+
 REM if this is not a git repo, and the application was installed from zip file we just want to run update
 REM if this is a cloned version of the git repo we want to pull from master, then run the update
 
@@ -16,7 +20,7 @@ if ERRORLEVEL 1 (
 	git pull origin %master_branch%
 )
 
-ping -n 1 github.com > NUL
+curl.exe -Is https://github.com > NUL
 if ERRORLEVEL 1 (
     echo "No internet connection. Skipping update." >> logs/start_dart.log
     del NULL
