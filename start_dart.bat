37b5019c7ebef2ffb21e84cff3ed86e6b88b200a
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2025-06-04 09:57:29 -0300

    checking if an update should be skipped b/c of no internet connection

--- start_dart.bat
+++ start_dart.bat
@@ -10,10 +10,19 @@ set /p dart_version=<current_version.txt
 REM if this is not a git repo, and the application was installed from zip file we just want to run update
 REM if this is a cloned version of the git repo we want to pull from master, then run the update
 
-git branch | find "* %master_branch%" > NULL & if not ERRORLEVEL 1 (
-	echo "Updating Application"
-	git pull origin %master_branch% >> logs/start_dart.log
+git branch | find "* %master_branch%" > NULL
+if ERRORLEVEL 1 (
+	git checkout %master_branch%
 )
 
-del NULL
-call .\update.bat
+ping -n 1 github.com > NUL
+if ERRORLEVEL 1 (
+    echo "No internet connection. Skipping update." >> logs/start_dart.log
+    del NULL
+    call .\server.bat
+) else (
+    echo "Updating Application" >> logs/start_dart.log
+    git pull origin %master_branch% >> logs/start_dart.log
+    del NULL
+    call .\update.bat
+)
