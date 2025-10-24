f0d9c5eeca8ea8ab2f021d49ebcf950ecf24a32a
Patrick Upson <upsonp@gmail.com>
2025-10-24 08:44:37 -0300

    updated settings.py to create a logs dir if it doesn't exist

--- config/settings.py
+++ config/settings.py
@@ -194,6 +194,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+log_dir = os.path.join(BASE_DIR, 'logs')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
 FORMATTERS = (
     {
         "simple": {
