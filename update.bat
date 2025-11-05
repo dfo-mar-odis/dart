8f41921fed04680e61c52da05f1eb39af17c66c2
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2025-11-05 08:07:02 -0400

    updated from cx_Oracle to oracledb and removed matplotlib from requirements.txt

--- update.bat
+++ update.bat
@@ -32,6 +32,7 @@ if not defined dart_version (
 if (%dart_version%==%update_version%) goto start_server
 
 echo "Updating Python Libraries, this may take several minutes"
+python -m pip install matplotlib --only-binary :all:
 python -m pip install -r .\requirements.txt
 
 :start_server
