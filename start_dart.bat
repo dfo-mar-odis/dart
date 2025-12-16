63a9b2c21e31a4e79d40952cdd6d090c2f035b79
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2025-12-16 14:20:28 -0400

    update to start_dart.bat for Instant Client check

--- start_dart.bat
+++ start_dart.bat
@@ -18,6 +18,15 @@ echo ============================================ >> logs/start_dart.log
 echo Version: %dart_version% >> logs/start_dart.log
 echo ============================================ >> logs/start_dart.log
 
+REM Check if Oracle Instant Client is in the CLASSPATH If not and the version number isn't empty
+REM we'll give the user a popup saying we can't update to 4.2.0+ and just start the server
+echo %CLASSPATH% | findstr /i "Oracle\12.2.0_Instant_x64" >nul
+if errorlevel 1 (
+    msg %username% "Oracle Instant Client 12 is required for DART 4.2.0+. Please install it from the DFO software center and run start_dart.bat again to update the applciation."
+    del NULL
+    call .\server.bat
+)
+
 REM if this is not a git repo, and the application was installed from zip file we just want to run update
 REM if this is a cloned version of the git repo we want to pull from master, then run the update
 
