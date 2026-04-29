b3bc2e3b1e90cba33db82623dd834c6907bc2905
Patrick Upson <Patrick.Upson@dfo-mpo.gc.ca>
2026-04-29 07:35:03 -0300

    updateding startup scripts

--- start_dart.bat
+++ start_dart.bat
@@ -30,13 +30,7 @@ REM if this is not a git repo, and the application was installed from zip file w
 REM    the update script to install python libraries and create the local DB
 REM if this is a cloned version of the git repo we want to pull from master, then run the update
 
-git branch | find "* %master_branch%" >NUL 2>&1
-if ERRORLEVEL 1 (
-    git checkout %master_branch%
-    git pull origin %master_branch%
-) else (
-    call .\update.bat
-)
+call git_update.bat %master_branch%
 
 REM Check to see if the user has access to github.com to see if the user has an internet connection
 REM     If they don't have an internet conection just start the server, if they do, run the update script
